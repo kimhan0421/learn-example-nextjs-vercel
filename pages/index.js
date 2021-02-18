94f34d11699632517a2868cef4ef44545f47babc
kim <kimhan0421@naver.com>
2021-02-18 09:54:55 +0900

    plz

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hi <a href="https://nextjs.org">Next.js!</a>
+          Hi <a href="https://nextjs.org">!</a>
         </h1>
       </main>
 
