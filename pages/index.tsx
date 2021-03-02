2101d2b21d5bd0466d63d29205a3103b4e00d549
kim <kimhan0421@naver.com>
2021-03-02 14:02:28 +0900

    Feat : add eslint config and styled components

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  font-size: 21px;
+  color: gray;
+`;
+
+const index: React.FC = () => {
+  return <Container>hi</Container>;
+};
+export default index;
